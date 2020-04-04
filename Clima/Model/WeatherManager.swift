d6c7e4305705cab7bca3240168727bcb52b0a5f8
keoki33 <gwong323@yahoo.com>
2020-04-04 18:53:24 +1100

    api fetch lesson done

--- Clima/Model/WeatherManager.swift
+++ Clima/Model/WeatherManager.swift
@@ -9,13 +9,33 @@
 import Foundation
 
 struct WeatherManager {
-    let weatherUrl = "http://api.openweathermap.org/data/2.5/weather?units=metric&appid=38fe7a8e5897d7609a473d87d3b8737d"
+    let weatherUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&appid=38fe7a8e5897d7609a473d87d3b8737d"
     
     func fetchWeather(cityName: String)  {
         let urlString = "\(weatherUrl)&q=\(cityName)"
-        print(urlString)
+performRequest(urlString: urlString)
+    }
+    
+    func performRequest(urlString: String)  {
+        if let url = URL(string: urlString) {
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url, completionHandler: handle(data:response:error:))
+            task.resume()
+        }
+        
+        
     }
     
     
+    func handle(data: Data?, response: URLResponse?, error: Error?) -> Void {
+        if error != nil {
+            print(error!)
+            return
+        }
+        if let safeData = data {
+            let dataString = String(data: safeData, encoding: .utf8)
+            print(dataString)
+        }
+    }
     
 }
